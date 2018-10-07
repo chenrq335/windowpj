19192740f73b1452e8e0f8024f285b9e64cbff9c
chenrq335 <1015099078@qq.com>
2018-10-07 15:22:32 +0800

    dao update

---
+++ src/com/szy/dao/GoodsDAO.java
@@ -0,0 +1,13 @@
+package com.szy.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoodsDAO {
+	
+	public List<String> findALl(){
+		List<String> lt=new ArrayList<String>();
+		return lt;
+	}
+
+}
