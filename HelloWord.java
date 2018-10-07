84a056d99ea027726061d17d7c78a2b21d12e1be
chenrq335 <43854955+chenrq335@users.noreply.github.com>
2018-10-07 15:14:16 +0800

    yyy
    yyyyy

---
+++ HelloWord.java
@@ -0,0 +1,16 @@
+package com.szy.entity;
+
+public class HelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello Word!");
+		
+		System.out.println("hello Word2!");
+	}
+
+}
